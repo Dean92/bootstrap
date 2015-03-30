2d372db50aace8f649c7aeb3381a16de47d6c484
Dean McCoy <dmccoy@osteopathic.org>
2015-03-30 17:03:57 -0500

    updates for Chapter2

---
+++ Northwind/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper.QueryableExtensions;
+using Northwind.Models;
+using Northwind.Controllers;
+
+namespace Northwind.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext_context;
+        private readonly ICurrent_currentUser;
+
+        public ProductsController(ApplicationDbContext context, ICurrentUser currentUser)
+        {
+            _context = context;
+            _currentUser = currentUser;
+        }
+
+        public ActionResult Index()
+        {
+            var models = ContextBoundObject.Products.Project().To<ProductViewModel>().ToArray();
+            return View(models);
+        }
+    }
+}
